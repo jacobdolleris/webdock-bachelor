df58a991822c39a8aebe3e3591108c99a7cd9592
Jacob Dolleris <J.dolleris@hotmail.com>
2023-12-01 11:00:08 +0100

    clean up 2

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,10 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <h1>Jooo</h1>
+    <h2>Hvordan går det?</h2>
+    <h3>Håber det går fint</h3>
+    <h4>Jajaja</h4>
+    <h5>Farvel!</h5>
   </header>
 
 
