1d6f6a104d92c5ac19ab03565d15187271ed0cd6
Jacob Dolleris <J.dolleris@hotmail.com>
2023-12-04 09:27:27 +0100

    Delete folder

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,56 @@
+<script setup>
+
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+
+<div class="top-menu-replacement ignore"><h2>About side</h2></div>
+
+  <main class="content">
+
+
+  <h1>:)</h1>
+
+
+
+  </main>
+
+
+  <div class="footer-replacement ignore"> <h2>Footer</h2></div>
+
 </template>
 
+
+<style lang="scss" scoped>
+
+.content{
+  height: 1000px;
+}
+
+h1{
+
+  font-size:50px;
+  text-align: center;
+  
+  padding: 50px;
+}
+.ignore{
+  height: 150px;
+  background-color: grey;
+
+  h2{
+    font-size: 30px;
+    text-align: center;
+    padding-top:30px;
+  }
+
+}
+.footer-replacement{
+  height: 400px;;
+}
+
+</style>
+
 <style>
+  @import './assets/mmain.css';
 </style>
