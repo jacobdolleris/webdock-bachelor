b20c795f2235dddf6e699d1676a65662f8fc3f86
FisterMedister <stefanjespersen8@gmail.com>
2023-12-06 13:39:22 +0100

    fixed modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,34 +1,89 @@
+<script setup>
+      defineProps({
+        btitle: String,
+        bmodel: String,
+        bprice: String,
+        mtitle: String
+    })
+</script>
+
 <template>
-    <div v-if="erÅbent" class="modal">
-      <a href="#" @click.prevent="lukModul">Luk</a>
-      <h1>{{ modalTitle }}</h1>
+  <div>
+    <a href="#" @click="openModal">
+      <div class="config-section-inner-option">
+        <h2>{{ btitle }}</h2>
+        <h3>{{ bmodel }}</h3>
+        <h3>{{ bprice }}</h3>
+      </div>
+    </a>
+
+  </div><!-- Button -->
+
+  <div v-if="isModalOpen" class="modal">
+    <div class="modal-content">
+      <h1>{{ mtitle }}</h1>
+      <a href="#" @click="closeModal">&times;</a>
     </div>
+    </div><!-- Modal -->
   </template>
   
-  <script>
-  export default {
-    props: {
-      erÅbent: Boolean,
-      modalTitle: String
+<script>
+export default {
+  data(){
+    return {
+      isModalOpen: false
+    };
+  },
+  methods: {
+    openModal() {
+      this.isModalOpen = true;
     },
-    methods: {
-      lukModul() {
-        this.$emit('luk-modul');
-      }
+    closeModal() {
+      this.isModalOpen = false;
     }
-  };
-  </script>
+  }
+}
+</script>
   
-  <style scoped>
-  .modal {
+<style lang="scss" scoped>
+$teal-color: #01A1A1;
+$orange-color:#EE8007;
+$webdock-green:#018971;
+$dark-green:#008661;
+$dark-grey-background:#2C404A;
+$grey:#666666;
+$dark-grey:#333333;
+.modal {
     position: fixed;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     padding: 20px;
-    background-color: #fff;
-    border: 1px solid #ccc;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  }
+    width: 850px;
+    height: 600px;
+    background-color:$dark-grey-background;
+    border-radius: 10px;
+    border: 3px solid $orange-color;
+  
+    a{
+        background-color:$orange-color;
+        padding:1%;
+        font-size: 50px;
+        width: 70px;
+        height: 30px;
+        border-radius: 10px;
+        float: right;
+
+        &:hover{
+            background-color: #cd6b03;
+        }
+    }
+
+.modal-content{
+    display: flex;
+    justify-content: space-between;
+}
+    
+  }// Modal
   </style>
   
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+import Modal from '../components/Modal.vue';
 </script>
 
 <template>
@@ -15,9 +15,6 @@
   <hr id="config">
  </header>
 
-
-
-
 <div class="DD-box">
   <div class="dropdown">
   <button class="dropdown-btn">Choose Carbinet</button>
@@ -30,95 +27,40 @@
 </div>
 <section class="config-section">
 
-
-
-
-
- 
-
-
-
 <div class="config-section-inner">
-  <div>
-    <a href="#" @click.prevent="openModul">
-      <div class="config-section-inner-option">
-        <h2>RAM</h2>
-        <h3>Model: ########</h3>
-        <h3>Pris: $$$</h3>
-      </div>
-    </a>
 
-  </div>
-
-  <div v-if="isModulOpen" class="modal">
-    <div class="modal-inner">
-      
-      <h1>Choose RAM</h1>
-      <a href="#" @click.prevent="closeModul">Close</a>
-    </div>
-    </div>
-
-
+  <Modal
+  btitle='RAM'
+  bmodel='Model: Stefan'
+  bprice='Price: 1000 $'
+  mtitle='Choose RAM'
+  />
 
   <div class="config-ection-inner-image">
-  </div>
+  </div><!-- INNER IMAGE -->
 
-  <a href="#" @click.prevent="openModul2">
-      <div class="config-section-inner-option">
-        <h2>CPU</h2>
-        <h3>Model: ########</h3>
-        <h3>Pris: $$$</h3>
-      </div>
-    </a>
-
-  </div>
-
-  <div v-if="isModul2Open" class="modal">
-    <div class="modal-inner">
-      
-      <h1>Choose CPU</h1>
-      <a href="#" @click.prevent="closeModul2">Close</a>
-    </div>
-    </div>
-
+  <Modal
+  btitle='CPU'
+  bmodel='Model: Jacob'
+  bprice='Price: 2000 $'
+  mtitle='Choose CPU'
+  />
+</div>
 
 <div class="config-section-inner">
-  <a href="#" @click.prevent="openModul3">
-    <div class="config-section-inner-option">
-    <h2>Port Speed</h2>
-    <h3>Model : ########</h3>
-    <h3>Price : $$$</h3>
-  </div>
-</a>
-  
-<div v-if="isModul3Open" class="modal">
-    <div class="modal-inner">
-      
-      <h1>Choose Port Speed</h1>
-      <a href="#" @click.prevent="closeModul3">Close</a>
-    </div>
-    </div>
+  <Modal
+  btitle='Port Speed'
+  bmodel='Model: Arni'
+  bprice='Price: 500 $'
+  mtitle='Choose Port Speed'
+  />
 
-  <a href="#" @click.prevent="openModul4">
-    <div class="config-section-inner-option">
-    <h2>DISCS</h2>
-    <h3>Model : ########</h3>
-    <h3>Price : $$$</h3>
-  </div>
-</a>
-
-
-
-<div v-if="isModul4Open" class="modal">
-    <div class="modal-inner">
-      
-      <h1>Choose DISCS</h1>
-      <a href="#" @click.prevent="closeModul4">Close</a>
-    </div>
-    </div>
-
-
-
+  <Modal
+  btitle='DISCS'
+  bmodel='Model: Søren'
+  bprice='Price: 50 $'
+  mtitle='Choose DISCS'
+  />
 
     </div>
     <div style="text-align: center;">
@@ -143,53 +85,53 @@
 
 
 <script>
-export default {
-  data() {
+// export default {
+//   data() {
     
     
-    return {
-      isModulOpen: false,
-      isModul2Open:false,
-      isModul3Open:false,
-      isModul4Open:false,
-      isModul5Open:false
-    };
-  },
-  methods: {
-    openModul() {
-      this.isModulOpen = true;
-    },
-    openModul2() {
-      this.isModul2Open = true;
-    },
-    openModul3() {
-      this.isModul3Open = true;
-    },
-    openModul4() {
-      this.isModul4Open = true;
-    },
-    openModul5() {
-      this.isModul5Open = true;
-    },
+//     return {
+//       isModulOpen: false,
+//       isModul2Open:false,
+//       isModul3Open:false,
+//       isModul4Open:false,
+//       isModul5Open:false
+//     };
+//   },
+//   methods: {
+//     openModul() {
+//       this.isModulOpen = true;
+//     },
+//     openModul2() {
+//       this.isModul2Open = true;
+//     },
+//     openModul3() {
+//       this.isModul3Open = true;
+//     },
+//     openModul4() {
+//       this.isModul4Open = true;
+//     },
+//     openModul5() {
+//       this.isModul5Open = true;
+//     },
   
 
-    closeModul() {
-      this.isModulOpen = false;
-    },
-    closeModul2() {
-      this.isModul2Open = false;
-    },
-    closeModul3() {
-      this.isModul3Open = false;
-    },
-    closeModul4() {
-      this.isModul4Open = false;
-    },
-    closeModul5() {
-      this.isModul5Open = false;
-    }
-  }
-};
+//     closeModul() {
+//       this.isModulOpen = false;
+//     },
+//     closeModul2() {
+//       this.isModul2Open = false;
+//     },
+//     closeModul3() {
+//       this.isModul3Open = false;
+//     },
+//     closeModul4() {
+//       this.isModul4Open = false;
+//     },
+//     closeModul5() {
+//       this.isModul5Open = false;
+//     }
+//   }
+// };
 </script>
 
 
