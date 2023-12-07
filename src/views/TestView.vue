106aa2f8c4a1b7a346218b3fa0ac642adef3181a
Jacob Dolleris <J.dolleris@hotmail.com>
2023-12-07 10:43:26 +0100

    Json data string

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,9 +1,35 @@
 <script>
+import data from '../assets/data.json';
+
+export default {
+  data() {
+    return {
+      server: data,
+    };
+  },
+  methods: {
+    formatData(options) {
+      return JSON.stringify(options, null, 2)
+        .replace(/["{}]/g, '') // Fjern dobbelte anførselstegn og krøllede parenteser
+        .replace(/,/g, ', '); // Tilføj et mellemrum efter kommaer
+    },
+  },
+};
 </script>
 
 <template>
-    <main>
-        <h1 style="color: black;">Testside</h1>
-    </main>
-    
-</template>
+    <div>
+      <p v-for="(value, key) in server.components.disks.user_selectable_options" :key="key">
+        {{ key }}: {{ value }}
+      </p>
+    </div>
+  </template>
+  
+
+
+  
+<style scoped>
+*{
+    color: black;
+}
+</style>
